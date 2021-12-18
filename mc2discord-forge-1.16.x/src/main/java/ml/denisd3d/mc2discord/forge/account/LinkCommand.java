257cf2d9a7dbb95fdcd3b64c9bb7477d74a502ce
DenisD3D <dd.denis2544@gmail.com>
2021-12-18 18:01:47 +0100

    Fix link/unlink command close #58

--- mc2discord-forge-1.16.x/src/main/java/ml/denisd3d/mc2discord/forge/account/LinkCommand.java
+++ mc2discord-forge-1.16.x/src/main/java/ml/denisd3d/mc2discord/forge/account/LinkCommand.java
@@ -22,17 +22,13 @@ public class LinkCommand {
         String[] strings = Mc2Discord.INSTANCE.config.account.link_command.trim().split(" ");
 
         LiteralArgumentBuilder<CommandSource> command = Commands.literal(strings[strings.length - 1]).executes(context -> {
-            if (!Mc2Discord.INSTANCE.config.account.link_command.trim().equals(context.getInput().substring(1))) {
-                context.getSource().sendFailure(new StringTextComponent("Mc2Discord settings have changed. Please use /" + Mc2Discord.INSTANCE.config.account.link_command.trim() + " instead. This command will be removed at next server restart"));
-            } else {
-                ServerPlayerEntity player = context.getSource().getPlayerOrException();
-                if (Mc2Discord.INSTANCE.m2dAccount != null) { // Should always be the case at this point
-                    String result = Mc2Discord.INSTANCE.m2dAccount.generateCodeOrNull(player.getGameProfile(), player.getGameProfile().getId());
-                    if (result != null) {
-                        context.getSource().sendSuccess(getCopiableTextComponent(result), false);
-                    } else {
-                        context.getSource().sendFailure(new StringTextComponent(Mc2Discord.INSTANCE.config.account.messages.link_error_already));
-                    }
+            ServerPlayerEntity player = context.getSource().getPlayerOrException();
+            if (Mc2Discord.INSTANCE.m2dAccount != null) { // Should always be the case at this point
+                String result = Mc2Discord.INSTANCE.m2dAccount.generateCodeOrNull(player.getGameProfile(), player.getGameProfile().getId());
+                if (result != null) {
+                    context.getSource().sendSuccess(getCopiableTextComponent(result), false);
+                } else {
+                    context.getSource().sendFailure(new StringTextComponent(Mc2Discord.INSTANCE.config.account.messages.link_error_already));
                 }
             }
             return 1;
