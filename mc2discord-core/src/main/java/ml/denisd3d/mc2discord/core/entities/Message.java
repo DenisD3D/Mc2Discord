925412d2a7cf41182ce08cc662f46e7e0ab9e05f
DenisD3D <dd.denis2544@gmail.com>
2022-06-24 19:44:32 +0200

    Fix server emoji not being correctly replaced by :name: form

--- mc2discord-core/src/main/java/ml/denisd3d/mc2discord/core/entities/Message.java
+++ mc2discord-core/src/main/java/ml/denisd3d/mc2discord/core/entities/Message.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Message extends Entity {
-    private static final Pattern p = Pattern.compile(" <:(.+?):\\d+>");
+    private static final Pattern emote_pattern = Pattern.compile("<:(.+?):\\d+>");
     public final HashMap<String, String> replacements = new HashMap<>();
     public String message;
 
@@ -17,11 +17,19 @@ public class Message extends Entity {
 
     @Override
     public String replace(String content) {
-        Matcher m = p.matcher(message);
-        if (m.find()) {
-            message = m.replaceFirst(":$1:");
+        Matcher matcher = emote_pattern.matcher(message);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            String replacement = matcher.group(1);
+            if (replacement != null) {
+                matcher.appendReplacement(buffer, "");
+                buffer.append(":").append(replacement).append(":");
+            }
         }
-        replacements.put("message", EmojiParser.parseToAliases(this.message));
+        matcher.appendTail(buffer);
+
+        replacements.put("message", this.message = EmojiParser.parseToAliases(buffer.toString()));
         return replace(content, null, replacements);
     }
 }
