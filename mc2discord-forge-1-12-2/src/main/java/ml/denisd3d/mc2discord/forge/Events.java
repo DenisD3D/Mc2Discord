7f014ccc0e6e5e51a0a3e89c694385f62a7ba1e4
DenisD3D <dd.denis2544@gmail.com>
2022-11-27 19:14:12 +0100

    Add /say, /me and /tellraw [@a, @s] commands (1.12.2)

--- mc2discord-forge-1-12-2/src/main/java/ml/denisd3d/mc2discord/forge/Events.java
+++ mc2discord-forge-1-12-2/src/main/java/ml/denisd3d/mc2discord/forge/Events.java
@@ -1,11 +1,21 @@
 package ml.denisd3d.mc2discord.forge;
 
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.MalformedJsonException;
+import ml.denisd3d.mc2discord.core.M2DUtils;
+import ml.denisd3d.mc2discord.core.Mc2Discord;
 import ml.denisd3d.mc2discord.core.entities.Advancement;
 import ml.denisd3d.mc2discord.core.entities.Death;
 import ml.denisd3d.mc2discord.core.entities.Player;
 import ml.denisd3d.mc2discord.core.events.MinecraftEvents;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.PlayerNotFoundException;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
@@ -15,6 +25,9 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.Optional;
+
+import static net.minecraft.command.CommandBase.buildString;
+import static net.minecraft.command.CommandBase.getChatComponentFromNthArg;
 
 @Mod.EventBusSubscriber(modid = "mc2discord", value = Side.SERVER)
 public class Events {
@@ -69,6 +82,55 @@ public class Events {
                             .getDescription()
                             .getUnformattedText())
             );
+        }
+    }
+
+    @SubscribeEvent
+    public static void onCommandEvent(CommandEvent event) {
+        if (!M2DUtils.canHandleEvent() || !Mc2Discord.INSTANCE.config.misc.relay_say_me_tellraw_command)
+            return;
+
+        String command_name = event.getCommand().getName();
+        if (command_name.equals("say") || command_name.equals("me") || command_name.equals("tellraw")) {
+            try {
+                boolean should_send_to_discord = true;
+                String message;
+                if (command_name.equals("tellraw")) {
+                    if (event.getParameters().length < 2)
+                        return;
+                    String target = event.getParameters()[0];
+                    String s = buildString(event.getParameters(), 1);
+
+                    if (target.equals("@s") && (event.getSender() == Mc2DiscordForge.commandSender)) {
+                        event.setCanceled(true); // Do not execute the vanilla command to prevent No player was found error but still return the message to discord
+                    } else if (!target.equals("@a")) {  // Else if the target is not everyone it does not target discord
+                        should_send_to_discord = false;
+                    }
+                    ITextComponent iTextComponent = ITextComponent.Serializer.jsonToComponent(s);
+                    if (iTextComponent == null)
+                        return;
+
+                    message = iTextComponent.getUnformattedText();
+                } else if (command_name.equals("me")) {
+                    if (event.getParameters().length <= 0)
+                        return;
+                    ITextComponent itextcomponent = getChatComponentFromNthArg(event.getSender(), event.getParameters(), 0, !(event.getSender() instanceof EntityPlayer));
+                    message = new TextComponentTranslation("chat.type.emote", event.getSender()
+                            .getDisplayName(), itextcomponent).getUnformattedText();
+                } else {
+                    if (event.getParameters().length <= 0 || event.getParameters()[0].length() <= 0)
+                        return;
+                    ITextComponent itextcomponent = getChatComponentFromNthArg(event.getSender(), event.getParameters(), 0, true);
+                    message = new TextComponentTranslation("chat.type.announcement", event.getSender()
+                            .getDisplayName(), itextcomponent).getUnformattedText();
+                }
+
+                if (should_send_to_discord) {
+                    Mc2Discord.INSTANCE.messageManager.sendInfoMessage(message);
+                }
+            }  // Ignore when the command is malformed
+            catch (IllegalArgumentException | CommandException | JsonSyntaxException ignored) {
+            }
         }
     }
 }
